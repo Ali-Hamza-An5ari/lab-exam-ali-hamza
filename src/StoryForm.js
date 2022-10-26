cff8413e732304ec7826aef246c028418a5c8e85
Ali-Hamza-An5ari <alihamza.bcss19@iba-suk.edu.pk>
2022-10-26 16:12:56 +0500

    Add a Loader

--- src/StoryForm.js
+++ src/StoryForm.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 
 function StoryForm() 
 {
-   const [searchRecs, setSearchRecs] = useState([""])
+    const [searchRecs, setSearchRecs] = useState([""])
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(undefined);
     const [q, setQ] = useState("")
+    const [spinner, setSpinner] = useState(false);
     const t = useRef()
+    var historyArr = []
 
     // useEffect( async () => {
     //     await fetch(q)
@@ -31,17 +33,39 @@ function StoryForm()
     {
         console.log(t.current.value)
         setSearchRecs(t.current.value)
-        console.log(searchRecs)
+        historyArr.push(t.current.value)
+        // console.log(searchRecs)
         setQ("https://hn.algolia.com/api/v1/search?"+t.current.value+"=&page=0")
-        fetchAPIData(t.current.value)
-        console.log(data)
+        console.log(q)
+        const url = "https://hn.algolia.com/api/v1/search?query=hello&page=0"
+        // const url = "https://hn.algolia.com/api/v1/search?"+t.current.value+"=&page=0"
+        fetchAPIData(url)
+        
+        
     }
-    function fetchAPIData(url) {
-        axios.get("https://hn.algolia.com/api/v1/search?query=hello&page=0").then(
+    async function  fetchAPIData(url) {
+        // await axios.get(url).then( res => this.setData(res.data)).then(function(res) {
+        //       alert();
+        //     })
+        //     .catch(function(error) {
+        //       console.log(error);
+        //     });;
+
+        axios.get(url).then(
             (response) => {
-                setData(response.data)
+                setData(response.data.results)
+                setSpinner(false)
             }
-        );
+        )
+        .then(function(response) {
+          alert();
+          setSpinner(true);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });;
+        console.log(url)
+        console.log(data)
      }
 
     // const handleClick = async() => {
@@ -55,12 +79,24 @@ function StoryForm()
     <>
         <h1>My Hacker stories</h1>
         <span>Search: <input ref={t} name="sBox"/> <button onClick={()=>{handle()}}>Submit</button></span>
+        {spinner && (
+        <p>something is loading</p>
+        )}
         {/* {
             data.map((e, index) =>
                  {
                     return  <div key={index}>{e.title}</div>
                 })
         } */}
+             {
+                historyArr.map(function(a, index)
+                    {
+                        return( <div key={index}>
+                            <button value={a}></button>
+                        </div>)
+                    })
+            }
+           
     </>
   )
 }
